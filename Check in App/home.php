8f9ca4b03176cb0793e8be52fa76268a9d8f85e9
turi667 <61826945+turi667@users.noreply.github.com>
2020-03-18 12:01:31 -0700

    Update home.php

--- Check in App/home.php
+++ Check in App/home.php
@@ -1,6 +1,6 @@
 <?php
 
-$connect = mysqli_connect("localhost", "root", "mysql123", "checkinapp");
+$connect = mysqli_connect("localhost", "root", "", "checkinapp");
 $columns = array(
     'first_name',
     'last_name',
